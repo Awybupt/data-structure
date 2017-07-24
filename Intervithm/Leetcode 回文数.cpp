66054b3d30e30e0cc21e1ad2ed8be7876a771355
Awy@bupt <wei.svip@foxmail.com>
2017-07-24 17:55:33 +0800

    Create Leetcode 回文数.cpp

---
+++ Intervithm/Leetcode 回文数.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+class solution{
+public:
+    bool ispalind(int s){
+        int t=s,an=0,i=0;
+        for(;t>0;t/=10)
+        {
+            an=10*an+t%10;
+            ++i;
+        }
+        for(int j;j<i/2;j++)
+        {
+            if(s%10!=an%10)
+                return false;
+        }
+        return true;
+    }
+};
+int main(void)
+{
+    solution ans;
+    int num=0;
+    std::cin>>num;
+    std::cout<<ans.ispalind(num);
+    return 0;
+}
